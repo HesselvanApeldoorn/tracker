583c6743d6a672724ab4ee735ab3e18e0f712a93
angor <antonis.gkortzis@gmail.com>
2014-03-20 11:32:01 +0100

    CustomLocation class added.. (is going to be used soon!)

---
+++ TrackerApp/src/ubicomp/tracker/CustomLocation.java
@@ -0,0 +1,50 @@
+package ubicomp.tracker;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Location;
+
+public class CustomLocation {
+	
+	public enum CustomLocationType{type1, type2, type3, typeX}
+	
+	private MarkerOptions markeroptions;
+	private int radius;
+	private int type;
+	private int numberOfVisits;
+	private long secondsSpent;
+	
+	public CustomLocation(MarkerOptions _marker, int _radius , int _type) {
+		this.markeroptions = _marker;
+		this.radius = _radius;
+		this.type = _type;
+		this.numberOfVisits = 0;
+	}
+	
+	public void IncreaseNumberOfVisits() {
+		this.numberOfVisits++;
+	}
+	
+	public boolean Overlap(CustomLocation _location) {
+		
+		Location storedLocation = new Location("");
+		storedLocation.setLatitude(this.markeroptions.getPosition().latitude);
+		storedLocation.setLongitude(this.markeroptions.getPosition().longitude);
+		
+		Location currentLocation = new Location("");
+		currentLocation.setLatitude(_location.markeroptions.getPosition().latitude);
+		currentLocation.setLongitude(_location.markeroptions.getPosition().longitude);
+		
+		float distance = Math.abs(storedLocation.distanceTo(currentLocation));
+		
+		return (distance <= radius);
+	}
+	
+	public int getRadius() {
+		return this.radius;
+	}
+	
+	
+}
