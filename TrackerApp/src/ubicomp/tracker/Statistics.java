54ea106a18f331ea9b2f23d32cf40b040e7e3fd5
githubva <hesselva1@gmail.com>
2014-03-24 14:42:40 +0100

    Added findhome options

--- TrackerApp/src/ubicomp/tracker/Statistics.java
+++ TrackerApp/src/ubicomp/tracker/Statistics.java
@@ -1,5 +1,7 @@
 package ubicomp.tracker;
 
+import java.util.Calendar;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import ubicomp.tracker.R;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 public class Statistics extends BaseMenu {
 	
+	static int HOMERADIUS = 100; // Average homesize
 	int timeSpentHome = 0;
 	LatLng locationHome;
 	CustomLocation customLocationHome = MainActivity.locationList.get(0);
@@ -29,28 +32,31 @@ public class Statistics extends BaseMenu {
 	}
 
 	private void findSpecialPlaces() {
-//		this.locationHome = this.findHome();
+		this.locationHome = this.findHome();
+		Toast.makeText(this, "latitude of home: " + this.locationHome.latitude + "",Toast.LENGTH_SHORT).show();
 	}
 
-//	private LatLng findHome() {
-//		//TODO pseudo code, not finished yet, probably crap
-//		int maxTimeSpent  = 0; // time in seconds
-//		for(int i=0; i<MainActivity.savedRoutes.size; i++) {
-//			if (MainActivity.savedRoutes[i].time > 18:00 && MainActivity.savedRoutes.time < 7:00) { // candidate home location, as you're usually at home between these times
-//				LatLng candidateLocationHome = MainActivity.savedRoutesList[i].location; // possible homelocation
-//				int timeSpent = 0;
-//				for(int j=0; j<MainActivity.savedLocations.size; j++) {
-//					if (abs(candidateLocationHome - MainActivity.savedLocations[j].location) < 100meter) { // routepiece is near the previous routepiece.
-//						timeSpent += MainActivity.savedRoutesList.get(j+1).time - MainActivity.savedRoutesList.get(j).time; // add time spent at a certain route piece
-//					}
-//				}
-//			}
-//			if (timeSpent > maxTimeSpent) { // better candidate for home location
-//				locationHome = candidateLocationHome;
-//			}	
-//		}
-//		return locationHome;
-//	}
+	private LatLng findHome() {
+		//TODO not verified yet. Should work in theory.
+		int maxTimeSpent  = 0; // time in milliseconds
+		LatLng locationHome = null;
+		for(int i=0; i<MainActivity.routesList.size()-1; i++) {
+			int timeSpent = 0;
+			LatLng candidateLocationHome = null;
+			if ((MainActivity.routesList.get(i).getDate().getHours() > 18) || (MainActivity.routesList.get(i+1).getDate().getHours()) < 7) { // candidate home location, as you're usually at home between these times
+				candidateLocationHome = MainActivity.routesList.get(i).getLocation(); // possible homelocation
+				for(int j=0; j<MainActivity.routesList.size()-1; j++) {
+					if  (this.inRadius(Statistics.HOMERADIUS, candidateLocationHome.latitude, candidateLocationHome.longitude, MainActivity.routesList.get(j).getLocation().latitude, MainActivity.routesList.get(j).getLocation().longitude)) {// routepiece is near the previous routepiece
+						timeSpent += (MainActivity.routesList.get(j+1).getDate().getTime() - MainActivity.routesList.get(j).getDate().getTime()); // add time spent at a certain route piece
+					}
+				}
+			}
+			if (timeSpent > maxTimeSpent) { // better candidate for home location
+				locationHome = candidateLocationHome;
+			}	
+		}
+		return locationHome;
+	}
 	
 
 	private void timeSpentInPlaces() {
@@ -58,13 +64,12 @@ public class Statistics extends BaseMenu {
 	}
 
 	private int calcTimeSpentHome() {
-		int timeSpent = 0;
+		int timeSpent = 0; // time in milliseconds
 		for(int i=0; i<MainActivity.routesList.size()-1; i++) { // iterate over all route pieces
 			TrackedRoute route = MainActivity.routesList.get(i); // get the location of a route piece
 			LatLng homeLocation = this.customLocationHome.getMarkerOptions().getPosition();
-			if (inRadius(this.customLocationHome.getRadius(), homeLocation.latitude, homeLocation.longitude, route.getLocation().latitude, route.getLocation().longitude)) {
+			if (this.inRadius(this.customLocationHome.getRadius(), homeLocation.latitude, homeLocation.longitude, route.getLocation().latitude, route.getLocation().longitude)) {
 				timeSpent += (MainActivity.routesList.get(i+1).getDate().getTime() - route.getDate().getTime()); // add time spent at a certain route piece
-				Toast.makeText(this, "time added: " + (MainActivity.routesList.get(i+1).getDate().getTime() - route.getDate().getTime()) + "",Toast.LENGTH_SHORT).show();
 			}
 		}
 		return timeSpent;
